584297a08ed61294700c3b2d3a8bcd7da1b0f1af
Joe Khaled <youssef.khaled.farag@gmail.com>
2022-09-20 11:50:52 +0200

    Initial Commit

---
+++ Payment-App/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Payment-App
+//
+//  Created by Youssef Khaled on 20/09/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
